1d70609abb7b090ce0327dbb0238cb806df97116
Chris Costello <cc@streeteasy.com>
2014-03-27 21:49:59 -0400

    A little easy start to Scala.

---
+++ problem001.scala
@@ -0,0 +1,12 @@
+object HelloWorld {
+  def main(args: Array[String]) {
+    var sum = 0;
+    for (x <- 1 to 999) {
+      // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+      if ((x % 3 == 0 ) || (x % 5 == 0)) {
+        sum += x;
+      }
+    }
+    println(s"$sum");
+  }
+}
