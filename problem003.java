a49392304dc36faffabbe1a65f721c235f37955f
Chris Costello <chris@chriscostello.org>
2014-03-29 18:32:25 -0400

    A basic factorer that works for numbers whose only divisors are prime.

---
+++ problem003.java
@@ -0,0 +1,31 @@
+import java.lang.Integer;
+import java.lang.String;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Factorer {
+	public static List<Integer> factorsForNumber(int number) {
+		List<Integer> factors = new ArrayList<Integer>();
+
+		if (number == 1) {
+			return Collections.singletonList(new Integer(1));
+		}
+
+		for (int i = 2; i < number; i++) {
+			if ((number % i) == 0) {
+				factors.add(i);
+				factors.addAll(factorsForNumber(number / i));
+			}
+		}
+		return factors;
+	}
+}
+
+public class problem003 {
+	public static void main(String[] args) {
+		for (Integer i : Factorer.factorsForNumber(1048576)) {
+			System.out.println(i);
+		}
+	}
+}
