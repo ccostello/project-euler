91982ca6367802602ca17d84ba84e7aaaf8dfc89
Chris Costello <chris@chriscostello.org>
2014-03-29 18:35:55 -0400

    Reindent.

--- problem003.java
+++ problem003.java
@@ -5,27 +5,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class Factorer {
-	public static List<Integer> factorsForNumber(int number) {
-		List<Integer> factors = new ArrayList<Integer>();
+  public static List<Integer> factorsForNumber(int number) {
+    List<Integer> factors = new ArrayList<Integer>();
 
-		if (number == 1) {
-			return Collections.singletonList(new Integer(1));
-		}
+    if (number == 1) {
+      return Collections.singletonList(new Integer(1));
+    }
 
-		for (int i = 2; i < number; i++) {
-			if ((number % i) == 0) {
-				factors.add(i);
-				factors.addAll(factorsForNumber(number / i));
-			}
-		}
-		return factors;
-	}
+    for (int i = 2; i < number; i++) {
+      if ((number % i) == 0) {
+        factors.add(i);
+        factors.addAll(factorsForNumber(number / i));
+      }
+    }
+    return factors;
+  }
 }
 
 public class problem003 {
-	public static void main(String[] args) {
-		for (Integer i : Factorer.factorsForNumber(1048576)) {
-			System.out.println(i);
-		}
-	}
+  public static void main(String[] args) {
+    for (Integer i : Factorer.factorsForNumber(1048576)) {
+      System.out.println(i);
+    }
+  }
 }
