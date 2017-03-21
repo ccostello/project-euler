337288df0419be312a8a466ebb655275838ddbd8
Chris Costello <chris@chriscostello.org>
2017-03-21 18:25:16 -0400

    I was bored when I wrote that last solution. This is cleaner.

--- problem003.java
+++ problem003.java
@@ -1,105 +1,42 @@
-import java.lang.Long;
-import java.lang.String;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
-
-// 1048576
-// 2, 524288
-//    2, 262144
-//       2, 131072
-//          2, 65536
-//             2, 32768
-//                2, 16384
-//                   2, 8192
-//                      2, 4096
-//                         2, 2048
-// ...
-
-class CompositeNumber {
-  private long number;
-  private CompositeNumber left;
-  private CompositeNumber right;
-  private boolean factored;
-
-  public CompositeNumber(long number) {
-    this.number = number;
-  }
-
-  public CompositeNumber getLeft() {
-    return left;
-  }
-
-  public CompositeNumber getRight() {
-    return right;
-  }
-
-  public long getValue() {
-    return number;
-  }
-
-  public boolean factored() {
-    return factored;
-  }
-
-  public boolean isPrime() {
-    return (factored() && getLeft() == null && getRight() == null);
-  }
-
-  public void factor() {
-    if (number == 1)
-      return;
-
-    for (long i = 2; i < number; i++) {
-      if ((number % i) == 0) {
-        this.left = new CompositeNumber(i);
-        this.right = new CompositeNumber(number/i);
-        this.left.factor();
-        this.right.factor();
-        break;
-      }
-    }
-
-    factored = true;
-  }
-
-  public List<Long> getFactors() {
-    List<Long> factors = new ArrayList<Long>();
-    if (getLeft().isPrime()) {
-      factors.add(getLeft().getValue());
-    } else {
-      factors.addAll(getLeft().getFactors());
-    }
-
-    if (getRight().isPrime()) {
-      factors.add(getRight().getValue());
-    } else {
-      factors.addAll(getRight().getFactors());
-    }
-
-    return factors;
-  }
-
-  // Now here's the implementation-specific bit. We always put the largest of the two factors on the right of the tree.
-  public long largestFactor() {
-    if (isPrime()) {
-      return getValue();
-    }
-    return getRight().largestFactor();
-  }
-}
 
 class Factorer {
   public static List<Long> factorsForNumber(long number) {
-    CompositeNumber composite = new CompositeNumber(number);
-    composite.factor();
-    return composite.getFactors();
+    List<Long> factors = new ArrayList<>();
+    long startFactor = 2;
+    while (startFactor < number) {
+      for (long x = startFactor; x <= number; x++) {
+        if ((number % x) == 0) {
+          number /= x;
+          factors.add(x);
+          startFactor = x;
+          break;
+        }
+      }
+    }
+    return factors;
   }
 
   public static long largestFactorForNumber(long number) {
-    CompositeNumber compositeNumber = new CompositeNumber(number);
-    compositeNumber.factor();
-    return compositeNumber.largestFactor();
+    if (number < 0) {
+      number *= -1;
+    }
+    if (number == 1L) {
+      return 1L;
+    }
+
+    long factor = 2;
+    while (factor < number) {
+      for (long x = factor; x <= number; x++) {
+        if ((number % x) == 0) {
+          number /= x;
+          factor = x;
+          break;
+        }
+      }
+    }
+    return factor;
   }
 }
 
